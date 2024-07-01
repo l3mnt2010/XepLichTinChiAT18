187aa4fb169b9ecd969efd9fa3b0310a77ce7126
llamtruongdev365 <126931874+llamtruongdev365@users.noreply.github.com>
2024-07-01 13:41:30 +0700

    feat: add correct tinchi

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="text-center text-sm">
+        <span class="font-bold mx-2">Nguồn: ngosangns</span>
+        <span class="font-bold mr-2">Clone ké by l3mnt2010 :v</span> <span><button class="p-2 font-bold bg-green-600 rounded-md" onclick="window.print()">Lưu về PDF</button></span>
+    </footer>
+</template>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+		width: 100%;
+		min-width: 60rem;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+	import externalLinkIcon from '../assets/external-link-icon.svg';
+</script>
+
+<template>
+    <header class="mt-12 mb-4 text-center">
+        <h1 class="text-3xl font-bold">
+            <a href="/">HỖ TRỢ XẾP LỊCH TÍN CHỈ KMA FROM KIT CLONE BY l3mnt2010</a>
+        </h1>
+    </header>
+</template>
+
+<style>
+	header {
+		width: 100%;
+		min-width: 60rem;
+	}
+</style>
